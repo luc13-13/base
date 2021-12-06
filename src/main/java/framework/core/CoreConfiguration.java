483e34b175d20e7e894f5f497eec0c1bbf246476
luc13-13 <lucheng@mail.bnu.edu.cn>
2021-12-06 20:03:58 +0800

    Initial commit

---
+++ src/main/java/framework/core/CoreConfiguration.java
@@ -0,0 +1,13 @@
+package framework.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: lucheng
+ * @data: 2021/11/27 22:39
+ * @version: 1.0
+ */
+public class CoreConfiguration {
+}
